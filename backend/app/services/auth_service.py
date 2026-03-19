1069fd92d665048959aa4ed054ccbfc3c9a8e638
MohammadOmerKhan <116611268+MohammadOmerKhan@users.noreply.github.com>
2026-03-18 20:24:50 -0400

    Update auth_service.py

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from app.repositories.user_repository import UserRepository   
 from app.schemas.user import UserCreate, Token 
 from app.utils.security import hash_password, verify_password, create_access_token 
+from app.models.user import User
 
 
 class AuthService:
@@ -37,12 +38,12 @@ class AuthService:
 
         if existing_user:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST
+                status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email already registered"
             )
 
 
-        new_user = UserCreate(
+        new_user = User(
                 email = data.email,
                 hashed_password = hash_password(data.password),
                 first_name = data.first_name,
